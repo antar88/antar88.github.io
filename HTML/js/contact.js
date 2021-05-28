d4f7aac24cd7b68b19e2c44757b285dbeb0806b8
Antar <antar.moratona@king.com>
2021-05-28 18:17:04 +0200

    css fixes

--- HTML/js/contact.js
+++ HTML/js/contact.js
@@ -4,11 +4,7 @@ $('#working_form').submit(function() {
 
     $("#message").slideUp(750, function() {
         $('#message').hide();
-
-        $('#submit')
-            .before('<img id="contact_spinner" src="" class="spinner" />')
-            .attr('disabled', 'disabled')
-            .attr('value', 'Sending...');
+        $('#submit').attr('disabled', 'disabled').attr('value', 'Sending...');
 
         $.post(action, {
                 name: $('#name').val(),
@@ -19,12 +15,11 @@ $('#working_form').submit(function() {
                 data = JSON.parse(data);
                 document.getElementById('message').innerHTML = data.html;
                 $('#message').slideDown('slow');
-                $('#contact_spinner').fadeOut('slow', function() {
-                    $(this).remove()
-                });
 
                 if ('success'.match(data.result) != null) {
                     $('#working_form').slideUp('slow');
+                    $('#contact_form_container').addClass('d-flex').addClass('align-items-center');
+                    $('#contact_form_container').find('div.contact_form').addClass('col-lg-12').addClass('align-middle');
                 }
                 else {
                     $('#submit').attr('value', 'Try again').removeAttr('disabled');
